e2dc35c19065a4422624a5c0c3d38c3d52571d01
JuscelinoMamede <juscemamede01@gmail.com>
2023-09-08 14:27:52 -0300

    :construction: Ajustes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function redirectToIndexHtml() {
   window.location.href = "/index.html";
 }
+
+function openNbaProjectPage() {
+  window.location.href = "pages/nbaProject.html";
+}
+function openGasStationPage() {
+  window.location.href = "pages/gasStation.html";
+}
+function expressoSaudePage() {
+  window.location.href = "pages/expressoSaude.html";
+}
